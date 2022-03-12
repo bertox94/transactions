72bb90e444bf72647b31618b34128f6f065b2ad9
bertox94 <10519773@polimi.it>
2022-03-12 15:09:26 +0100

    OK

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,25 +20,44 @@ import java.util.Properties;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
+        initialize_processor();
+        initialize_DB_connection();
+        SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("Initialization completed.");
+
+    }
+
+    public static boolean initialize_processor() {
         try {
             Files.delete(Paths.get("processor\\main.exe"));
         } catch (Exception ignored) {
+            //no problem
         }
 
         try {
             Files.delete(Paths.get("main.obj"));
         } catch (Exception ignored) {
+            //no problem
         }
 
-        String[] command = {"cmd.exe", "/C", "Start /B", "E:\\workspace\\webapp-transaction-scheduler\\processor\\build.bat"};
-        Runtime.getRuntime().exec(command);
-        while (Files.notExists(Paths.get("processor\\main.exe"))) {
+        try {
+            String[] command = {"cmd.exe", "/C", "Start /B", "E:\\workspace\\webapp-transaction-scheduler\\processor\\build.bat"};
+            Runtime.getRuntime().exec(command);
+            while (Files.notExists(Paths.get("processor\\main.exe"))) {
+            }
+            Thread.sleep(500);
+            Runtime.getRuntime().exec("E:\\workspace\\webapp-transaction-scheduler\\processor\\main.exe", null, new File("E:\\workspace\\webapp-transaction-scheduler\\processor"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-        Thread.sleep(500);
-        Runtime.getRuntime().exec("E:\\workspace\\webapp-transaction-scheduler\\processor\\main.exe", null, new File("E:\\workspace\\webapp-transaction-scheduler\\processor"));
-        Thread.sleep(500);
+        return true;
+    }
 
+    public static boolean initialize_DB_connection() {
         try {
             String url = "jdbc:postgresql://otto.db.elephantsql.com/olkpwuzc";
             Properties props = new Properties();
@@ -48,13 +67,10 @@ public class DemoApplication {
             MainController.connection = DriverManager.getConnection(url, props);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            return false;
         }
-
-        SpringApplication.run(DemoApplication.class, args);
-
-
-        System.out.println("Initialization completed.");
-
+        return true;
     }
+
 }
 
