9eb8d56d507df9c15388b45d3364a11e896ae3d7
bertox94 <10519773@polimi.it>
2022-03-27 20:53:18 +0200

    OK

--- queries for charts.sql
+++ queries for charts.sql
@@ -15,56 +15,70 @@ group by executiondate
 order by executiondate;
 
 
-
-select _num / _denom as m
+select m,
+       (select avg(balance)
+        from (select sum(balance) as balance
+              from preview p
+              group by executiondate
+              order by executiondate
+             ) as subq_b
+       ) - m * (select (count(*) + 1) / 2.0
+                from (
+                         select distinct executiondate
+                         from preview p
+                     ) as subq2
+       ) as q
 from (
-         select sum(balance_a * x_a) as _num
+         select _num / _denom as m
          from (
-                  select balance - (select avg(balance)
-                                    from (
-                                             select sum(balance) as balance
-                                             from preview p
-                                             group by executiondate
-                                             order by executiondate
-                                         ) as subq1) as balance_a,
-                         x_a
+                  select sum(balance_a * x_a) as _num
                   from (
-                           select sum(balance) as balance, executiondate
-                           from preview p
-                           group by executiondate
-                           order by executiondate
-                       ) as subq2
-                           full outer join (
-                      select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0
-                                                                           from (
-                                                                                    select distinct executiondate
-                                                                                    from preview p
-                                                                                ) as subq2
-                      ) as x_a,
-                             executiondate
-                      from (
-                               select distinct executiondate
-                               from preview p
-                           ) as subq1
-                  ) as subq3 on subq2.executiondate = subq3.executiondate
-                  where subq2.executiondate is not null
-                     or subq3.executiondate is not null
-              ) as subq4
-     ) as sub6,
-     (
-         select sum(x_a ^ 2) as _denom
-         from (
-                  select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0
-                                                                       from (
-                                                                                select distinct executiondate
-                                                                                from preview p
-                                                                            ) as subq2) as x_a
+                           select balance - (select avg(balance)
+                                             from (
+                                                      select sum(balance) as balance
+                                                      from preview p
+                                                      group by executiondate
+                                                      order by executiondate
+                                                  ) as subq1) as balance_a,
+                                  x_a
+                           from (
+                                    select sum(balance) as balance, executiondate
+                                    from preview p
+                                    group by executiondate
+                                    order by executiondate
+                                ) as subq2
+                                    full outer join (
+                               select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0
+                                                                                    from (
+                                                                                             select distinct executiondate
+                                                                                             from preview p
+                                                                                         ) as subq2
+                               ) as x_a,
+                                      executiondate
+                               from (
+                                        select distinct executiondate
+                                        from preview p
+                                    ) as subq1
+                           ) as subq3 on subq2.executiondate = subq3.executiondate
+                           where subq2.executiondate is not null
+                              or subq3.executiondate is not null
+                       ) as subq4
+              ) as sub6,
+              (
+                  select sum(x_a ^ 2) as _denom
                   from (
-                           select distinct executiondate
-                           from preview p
-                       ) as subq1
-              ) as subq5
-     ) as sub7;
+                           select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0
+                                                                                from (
+                                                                                         select distinct executiondate
+                                                                                         from preview p
+                                                                                     ) as subq2) as x_a
+                           from (
+                                    select distinct executiondate
+                                    from preview p
+                                ) as subq1
+                       ) as subq5
+              ) as sub7
+     ) as sub14;
 
 
 -- QUERIES FOR THE SECOND CHART
