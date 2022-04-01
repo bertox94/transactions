577dd96bdb03e5061894fa39e033a999755ec7e0
bertox94 <10519773@polimi.it>
2022-04-01 21:44:20 +0200

    OK

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -78,23 +78,123 @@ public class MainController {
                         .append(Objects.equals(tokens[1], " null") ? "NULL" : "'" + tokens[1] + "'").append(", ")
                         .append(Objects.equals(tokens[2], " null") ? "NULL" : "'" + tokens[2] + "'").append(", ")
                         .append(tokens[3]).append(", ")
-                        .append(tokens[4]).append("),");
+                        .append(tokens[4]).append("), ");
             }
-            VALUES = new StringBuilder(VALUES.substring(0, VALUES.length() - 1));
+            VALUES.deleteCharAt(VALUES.length() - 2);
             VALUES.append(";");
 
             stmt.executeUpdate(
                     "INSERT INTO " + TABLENAME + " (descr, planneddate, executiondate, amount, balance) VALUES " + VALUES);
 
             //QUERY FOR THE TABLE
-            stmt.executeQuery("select *\n" +
-                    "from " + TABLENAME + " \n" +
-                    "where planneddate is not null;");
+            ResultSet rs = stmt.executeQuery("select * " +
+                    " from " + TABLENAME +
+                    " where planneddate is not null; ");
+
+            resp = "{\"html\":[";
+
+            if (rs.next()) {
+                resp += "{\"executiondate\":\"" + rs.getString(1) + "\",";
+                resp += "\"planneddate\":\"" + rs.getString(2) + "\",";
+                resp += "\"descr\":\"" + rs.getString(3) + "\",";
+                resp += "\"amount\":\"" + rs.getString(4) + "\",";
+                resp += "\"balance\":\"" + rs.getString(5) + "\"}";
+            }
+            while (rs.next()) {
+                resp += ",{\"executiondate\":\"" + rs.getString(1) + "\",";
+                resp += "\"planneddate\":\"" + rs.getString(2) + "\",";
+                resp += "\"descr\":\"" + rs.getString(3) + "\",";
+                resp += "\"amount\":\"" + rs.getString(4) + "\",";
+                resp += "\"balance\":\"" + rs.getString(5) + "\"}";
+            }
+            resp += "]}";
 
             //QUERIES FOR THE FIRST CHART
-            stmt.executeQuery("select distinct executiondate\n" +
-                    "from " + TABLENAME + ";");
+            stmt.executeQuery(" select distinct executiondate " +
+                    " from " + TABLENAME + ";");
 
+            stmt.executeQuery(" select sum(balance) as balance " +
+                    " from preview p " +
+                    " group by executiondate " +
+                    " order by executiondate; ");
+            stmt.executeQuery(" select m, " +
+                    "        (select avg(balance) " +
+                    "         from (select sum(balance) as balance " +
+                    "               from preview p " +
+                    "               group by executiondate " +
+                    "               order by executiondate " +
+                    "              ) as subq_b " +
+                    "        ) - m * (select (count(*) + 1) / 2.0 " +
+                    "                 from ( " +
+                    "                          select distinct executiondate " +
+                    "                          from preview p " +
+                    "                      ) as subq2 " +
+                    "        ) as q " +
+                    " from ( " +
+                    "          select _num / _denom as m " +
+                    "          from ( " +
+                    "                   select sum(balance_a * x_a) as _num " +
+                    "                   from ( " +
+                    "                            select balance - (select avg(balance) " +
+                    "                                              from ( " +
+                    "                                                       select sum(balance) as balance " +
+                    "                                                       from preview p " +
+                    "                                                       group by executiondate " +
+                    "                                                       order by executiondate " +
+                    "                                                   ) as subq1) as balance_a, " +
+                    "                                   x_a " +
+                    "                            from ( " +
+                    "                                     select sum(balance) as balance, executiondate " +
+                    "                                     from preview p " +
+                    "                                     group by executiondate " +
+                    "                                     order by executiondate " +
+                    "                                 ) as subq2 " +
+                    "                                     full outer join ( " +
+                    "                                select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0 " +
+                    "                                                                                     from ( " +
+                    "                                                                                              select distinct executiondate " +
+                    "                                                                                              from preview p " +
+                    "                                                                                          ) as subq2 " +
+                    "                                ) as x_a, " +
+                    "                                       executiondate " +
+                    "                                from ( " +
+                    "                                         select distinct executiondate " +
+                    "                                         from preview p " +
+                    "                                     ) as subq1 " +
+                    "                            ) as subq3 on subq2.executiondate = subq3.executiondate " +
+                    "                            where subq2.executiondate is not null " +
+                    "                               or subq3.executiondate is not null " +
+                    "                        ) as subq4 " +
+                    "               ) as sub6, " +
+                    "               ( " +
+                    "                   select sum(x_a ^ 2) as _denom " +
+                    "                   from ( " +
+                    "                            select ROW_NUMBER() OVER (ORDER BY executiondate) - (select (count(*) + 1) / 2.0 " +
+                    "                                                                                 from ( " +
+                    "                                                                                          select distinct executiondate " +
+                    "                                                                                          from preview p " +
+                    "                                                                                      ) as subq2) as x_a " +
+                    "                            from ( " +
+                    "                                     select distinct executiondate " +
+                    "                                     from preview p " +
+                    "                                 ) as subq1 " +
+                    "                        ) as subq5 " +
+                    "               ) as sub7 " +
+                    "      ) as sub14; ");
+
+            //QUERIES FOR THE SECOND CHART
+            stmt.executeQuery(" select executiondate " +
+                    " from preview p " +
+                    " where planneddate is not null;");
+            stmt.executeQuery(" select amount " +
+                    " from preview p " +
+                    " where planneddate is not null;");
+            stmt.executeQuery(" select balance " +
+                    " from preview p " +
+                    " where planneddate is not null;");
+
+            stmt.executeUpdate("drop table " + TABLENAME + " ;");
+            preview_id.decrementAndGet();
 
         } catch (SQLException e) {
             e.printStackTrace();
